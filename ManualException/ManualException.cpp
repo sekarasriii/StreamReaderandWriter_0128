c39d70ec1285a8a0683fc727bc7505eca42e0e8d
sekarasriii <sekareeesh@gmail.com>
2024-06-03 10:36:19 +0700

    membuat try catch

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -7,6 +7,16 @@ using namespace std;
 
 int main() {
 	try {
-		cout << 
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "Default pengecualian dieksekusi" << endl;
 	}
 }
